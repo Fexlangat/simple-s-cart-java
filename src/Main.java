dcf3268d3bea2a85edb66316067cc8cef9d35c9f
Felix Langat <fexlangat@gmail.com>
2025-04-23 11:46:06 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
+
+
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String item;
+        Double quantity;
+        String currency ="$";
+        Double price;
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Hallo! What would you like to buy?");
+        item = scanner.next();
+
+        System.out.printf("How many!");
+        quantity = scanner.nextDouble();
+
+        System.out.printf("How much!");
+         price = scanner.nextDouble();
+
+        Double total = price * quantity;
+
+        System.out.printf("Your total amount is :" + item +" " + currency+" " + total );
+
+
+
+
+
+        System.out.printf("Thank you and welcome again!!!!!");
+
+
+
+        scanner.close();
+
+
     }
 }
